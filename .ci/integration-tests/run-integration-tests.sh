6db1d423a2aab220edf489a0e276f66041084a76
apldev3 <apl.dev3@jhuapl.edu>
2018-12-26 13:24:55 -0500

    [#46] Allow for detection of complete Integration Environment Setup

---
+++ .ci/integration-tests/run-integration-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script to run the Integration Tests for HIRS
+
+set -e
+
+# Start Integration Testing Docker Environment
+#docker-compose -f .ci/docker/docker-compose.yml up -d
+
+# Check to see if Environment Stand-Up is Complete
+# TODO: Refine to handle multiple container IDs
+container_id_regex='([a-f0-9]{12})\s+hirs\/hirs-ci:tpm2provisioner'
+while : ; do
+    docker_containers=$(docker container ls)
+    if [[ $docker_containers =~ $container_id_regex ]]; then
+        container_id=${BASH_REMATCH[1]}
+        break
+    fi
+done
+
+tpm2_provisioner_started_regex='TPM2 Provisioner Loaded!'
+while : ; do
+    docker_logs=$(docker logs $container_id)
+    if [[ $docker_logs =~ $tpm2_provisioner_started_regex ]]; then
+        break
+    fi
+done
+
+echo "Environment Stand-Up Complete!"
