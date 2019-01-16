b2833dd3aa3ce52dbb91939c98230e8f30dc896c
apldev3 <apl.dev3@jhuapl.edu>
2019-01-16 15:58:26 -0500

    [#71] Try a new direction for setting up TPM 1.2 Provisioner Testing

--- .ci/integration-tests/setup-tpmprovisioner.sh
+++ .ci/integration-tests/setup-tpmprovisioner.sh
@@ -5,26 +5,15 @@
 set -e
 
 echo "START TPMPROVISIONER SCRIPT!!!!!!!!!!!!!!!!!!!!!"
-ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /lib64/libcrypto.so.1.0.0
 
-# Attempt to build TPM Emulator
-pushd /tpm_emulator
-cd tpm-emulator-0.7.5 && ./build.sh
+ls -la /dev/
 
-echo "Build complete!"
-cd build && make install
-popd
+# ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /lib64/libcrypto.so.1.0.0
+echo "Starting TrouSerS Daemon"
+tcsd
 
-echo "Finished installing"
-
-# Initialize TPM Emulator
-echo "Attempting modprobe"
-modprobe tpmd_dev &> /tpm_emulator/test-file
-echo $?
-/usr/local/bin/tpmd &>> /tpm_emulator/test-file
-echo $?
-tcsd &>> /tpm_emulator/test-file
-echo $?
+echo "Testing TPM Connectivity"
+tpm_selftest
 
 # Package and install HIRS TPM 1.2 Provisioner
 pushd /HIRS
