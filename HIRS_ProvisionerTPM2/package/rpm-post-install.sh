4e39b6856b119f55b63fc52d3a3bf1b685c6027c
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2020-10-09 13:18:10 -0400

    post install script was run however that is all that was run.  The set tcg properties script was not included.  The contents of the new script have been added to the post install script and the set tcg script has been removed.

--- HIRS_ProvisionerTPM2/package/rpm-post-install.sh
+++ HIRS_ProvisionerTPM2/package/rpm-post-install.sh
@@ -40,6 +40,23 @@ DEFAULT_SITE_CONFIG_FILE
 fi
 ln -s -f /etc/hirs/provisioner/hirs-provisioner.sh /usr/sbin/hirs-provisioner
 
-if [ ! -f "set_tcg_properties.sh" ]; then
-  sh ./set_tcg_properties.sh
+TCG_BOOT_FILE="/etc/hirs/tcg_boot.properties"
+MAINFEST_DIRECTORY="/boot/tcg/manifest"
+LOG_FILE_LOCATION="$MAINFEST_DIRECTORY/rim/"
+TAG_FILE_LOCATION="$MAINFEST_DIRECTORY/swidtag/"
+
+if [ ! -f "$TCG_BOOT_FILE" ]; then
+  touch "$TCG_BOOT_FILE"
 fi
+
+if [ -d "$LOG_FILE_LOCATION" ]; then
+  RIM_FILE=$(find "$LOG_FILE_LOCATION" -name '*.rimel' -or -name '*.bin' -or -name '*.rimpcr' -or -name '*.log')
+  echo "tcg.rim.file=$RIM_FILE" >> "$TCG_BOOT_FILE"
+fi
+
+if [ -d "$TAG_FILE_LOCATION" ]; then
+  SWID_FILE=$(find "$TAG_FILE_LOCATION" -name '*.swidtag')
+  echo "tcg.swidtag.file=$SWID_FILE" >> "$TCG_BOOT_FILE"
+fi
+
+chmod -w "$TCG_BOOT_FILE"
