c4741b869cd2b1e2fb86af32ef8f01c3e8255799
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2021-03-09 07:27:35 -0500

    Removed unused import

--- HIRS_AttestationCA/src/main/java/hirs/attestationca/AbstractAttestationCertificateAuthority.java
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/AbstractAttestationCertificateAuthority.java
@@ -92,7 +92,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
