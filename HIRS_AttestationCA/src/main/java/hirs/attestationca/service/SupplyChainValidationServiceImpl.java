688b8eeb0d7f58b6368f07035bffc0cc53e90c32
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2021-06-23 08:56:14 -0400

    Removed some spaces

--- HIRS_AttestationCA/src/main/java/hirs/attestationca/service/SupplyChainValidationServiceImpl.java
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/service/SupplyChainValidationServiceImpl.java
@@ -695,7 +695,6 @@ public class SupplyChainValidationServiceImpl implements SupplyChainValidationSe
         }
     }
 
-    
     private SupplyChainValidation validatePlatformCredentialAttributes(
             final PlatformCredential pc, final DeviceInfoReport deviceInfoReport,
             final EndorsementCredential ec) {
