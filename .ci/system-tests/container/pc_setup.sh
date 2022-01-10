3bd2379078c49bd204b6c0093538987f7be75910
iadgovuser26 <33069955+iadgovuser26@users.noreply.github.com>
2022-01-10 15:13:51 -0500

    merged with provisioner-printerror-fix

--- .ci/system-tests/container/pc_setup.sh
+++ .ci/system-tests/container/pc_setup.sh
@@ -12,22 +12,22 @@ mkdir -p /boot/tcg/cert/platform/;  # Create the platform cert folder if its not
 rm -f /boot/tcg/cert/platform/*;   # clear out any previous data
 
 # set the folder to read the platform cert from
-echo "tcg.cert.dir=/boot/tcg/cert/platform/" >  /etc/hirs/tcg_boot.properties
+#echo "tcg.cert.dir=/boot/tcg/cert/platform/" >  /etc/hirs/tcg_boot.properties
 
-echo "Test is using platform cert(s) from $profile : $test"
+#echo "Test is using platform cert(s) from $profile : $test"
 # Step 1: Copy allcomponents script to the paccor/scripts folder
 cp -f /HIRS/.ci/system-tests/profiles/$profile/$test/$compscript /opt/paccor/scripts/allcomponents.sh;
 
 # Step 2: Copy allcomponents json file to the paccor/scripts folder
 cp  -f /HIRS/.ci/system-tests/profiles/$profile/$test/$hwlist /opt/paccor/scripts/$hwlist ;
 
-# Step 3: Copy the platform cert to tcg folder on boot drive  
+# Step 3: Copy the platform cert to tcg folder on boot drive
 pushd /HIRS/.ci/system-tests/profiles/$profile/$test/platformcerts/ > /dev/null
 
 for cert in * ; do
           cp -f $cert /boot/tcg/cert/platform/$cert;
     done
-    
+
     echo "contents of /boot/tcg/cert/platform/ is $(ls /boot/tcg/cert/platform/)"
 #    echo "contents of hirs config is $(ls -al /etc/hirs)"
     echo "contents of tcg config is $(cat /etc/hirs/tcg_boot.properties)"
