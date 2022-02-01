c110dade772702acdb2d95c5c83efe1b1122a09f
lareine <lareine@evoforge.org>
2022-02-01 18:27:05 -0500

    initial Platfom Cert test

--- .ci/system-tests/container/pc_setup.sh
+++ .ci/system-tests/container/pc_setup.sh
@@ -13,11 +13,23 @@ rm -f /boot/tcg/cert/platform/*;   # clear out any previous data
 
 echo "Test is using platform cert(s) from $profile : $test"
 
-# Step 1: Copy allcomponents script to the paccor/scripts folder
-cp -f /HIRS/.ci/system-tests/profiles/$profile/$test/$compscript /opt/paccor/scripts/allcomponents.sh;
+# Step 1: Copy allcomponents script to the paccor/scripts folder if there is one.
+# Use the default if test does not have a test specific file.
 
-# Step 2: Copy allcomponents json file to the paccor/scripts folder
-cp  -f /HIRS/.ci/system-tests/profiles/$profile/$test/$hwlist /opt/paccor/scripts/$hwlist ;
+allCompScript=/HIRS/.ci/system-tests/profiles/$profile/$test/$compscript
+if [ ! -f "$allCompFile" ]; then
+  allCompScript=/HIRS/.ci/system-tests/profiles/"$profile"/default/"$profile"_default_allcomponents.sh
+fi
+cp -f $allCompScript /opt/paccor/scripts/allcomponents.sh;
+
+# Step 2: Copy allcomponents json file to the paccor/scripts folder if there is one
+# Use the default if test does not have a test specific file.
+
+allCompJson=/HIRS/.ci/system-tests/profiles/$profile/$test/$hwlist /opt/paccor/scripts/$hwlist ;
+if [ ! -f "$allCompJson" ]; then
+      allCompJson=/HIRS/.ci/system-tests/profiles/"$profile"/default/"$profile"_default_hw.json
+fi
+cp  -f $allCompJson /opt/paccor/scripts/$hwlist ;
 
 # Step 3: Copy the platform cert to tcg folder on boot drive
 pushd /HIRS/.ci/system-tests/profiles/$profile/$test/platformcerts/ > /dev/null
