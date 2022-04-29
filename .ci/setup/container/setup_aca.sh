625ecd06f27cb46c19c4e6b8e487b52c749621ab
iadgovuser26 <33069955+iadgovuser26@users.noreply.github.com>
2022-04-29 14:14:46 -0400

    updated workflow for use with github packages

--- .ci/setup/container/setup_aca.sh
+++ .ci/setup/container/setup_aca.sh
@@ -6,9 +6,22 @@ set -e
 
 # Prevent rebuild of packages if they already exist
 cd /HIRS
-echo "building and packagin the ACA"
+echo "Building and packaging the ACA"
 if [ ! -d package/rpm/RPMS ]; then
-    ./package/package.centos.sh
+     mkdir -p /HIRS/logs/aca/
+     sh package/package.centos.sh &> /HIRS/logs/aca/aca_build.log
 fi
-echo "installing the ACA"
-yum install -y package/rpm/RPMS/noarch/HIRS_AttestationCA*.el7.noarch.rpm
+echo "Building and packaging the ACA completed"
+echo "Installing the ACA"
+yum install -y package/rpm/RPMS/noarch/HIRS_AttestationCA*.el7.noarch.rpm &> /HIRS/logs/aca/aca_install.log
+filename=package/rpm/RPMS/noarch/HIRS_AttestationCA*.el7.noarch.rpm
+echo "================================================================================"
+echo "Installing:"
+echo "       HIRS_AttestationCA"
+echo "                   $filename" 
+echo ""
+echo "Transaction Summary"
+echo "================================================================================"
+echo "Install  1 Package"
+echo ""
+echo "********************* End of ACA installation *********************"m
