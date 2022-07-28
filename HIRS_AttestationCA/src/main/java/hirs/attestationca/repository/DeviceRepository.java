5ec4d8e415b86e393dcbf66440d67d7cfaffcf91
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2022-07-28 07:41:54 -0400

    Reloaded the unit test

---
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/repository/DeviceRepository.java
@@ -0,0 +1,12 @@
+package hirs.attestationca.repository;
+
+import org.springframework.stereotype.Repository;
+
+//import org.springframework.data.jpa.respository.JpaRepository;
+
+/**
+ * Setting up for new creation for CRUD operations.
+ */
+@Repository
+public interface DeviceRepository { // extends JpaRepository<Device, UUID> {
+}
