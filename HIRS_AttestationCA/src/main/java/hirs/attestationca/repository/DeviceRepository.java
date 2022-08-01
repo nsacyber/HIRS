8dfd8a7484ae2a83d956cfee279d5b44d9781b55
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2022-08-01 08:39:21 -0400

    Finally have the jpa object import state working

--- HIRS_AttestationCA/src/main/java/hirs/attestationca/repository/DeviceRepository.java
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/repository/DeviceRepository.java
@@ -1,12 +1,15 @@
 package hirs.attestationca.repository;
 
+import hirs.data.persist.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//import org.springframework.data.jpa.respository.JpaRepository;
+import java.util.UUID;
+
 
 /**
  * Setting up for new creation for CRUD operations.
  */
 @Repository
-public interface DeviceRepository { // extends JpaRepository<Device, UUID> {
+public interface DeviceRepository extends JpaRepository<Device, UUID> {
 }
