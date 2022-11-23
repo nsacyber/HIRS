ae2b31f1ff1e2cbc8e87432f690c520931c1aa8d
iadgovuser26 <iadgovuser26@empire.eclipse.ncsc.mil>
2022-11-23 11:16:33 -0500

    added xjc generation to HIRS_Utils build.gradle

--- package/package.rocky.sh
+++ package/package.rocky.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-
-echo "Cleaning build/distributions"
-rm build/distributions/*
+portalPackDir="HIRS_AttestationCAPortal/build/distributions"
+hirsPackDir="build/distributions"
+mkdir -p $hirsPackDir
+if [ -n "$(ls -A $hirsPackDir 2>/dev/null)" ]; then
+  echo "Cleaning $hirsPackDir"
+  rm $hirsPackDir/*
+fi
 echo "Building HIRS_AttestationCA:war ..."
 ./gradlew :HIRS_AttestationCA:clean :HIRS_AttestationCA:war
 echo "Building HIRS_AttestationCAPortal.war..."
 ./gradlew :HIRS_AttestationCAPortal:clean :HIRS_AttestationCAPortal:buildRpm
 echo "Packaging HIRS_AttestationCA.rpm"
-cp HIRS_AttestationCAPortal/build/distributions/*  build/distributions/
-
+if [ -n "$(ls -A $portalPackDir 2>/dev/null)"  ]; then
+  cp $portalPackDir/*  $hirsPackDir/.
+else echo "Error: no package found in $packDir"
+fi
