eae9e3722886501a64b22ada32f59df712341794
iadgovuser26 <iadgovuser26@empire.eclipse.ncsc.mil>
2023-01-18 16:27:02 +0000

    Updated ACA Portal build.gradle [no ci]
     Committer: iadgovuser26

--- package/scripts/common/db_create.sh
+++ package/scripts/common/db_create.sh
@@ -10,22 +10,25 @@ fi
 
 echo "Creating HIRS Database..."
 
-if [ $DOCKER_CONTAINER  = true ]; then
-   # If in Docker container, avoid services that invoke the D-Bus
-    if [[ $(pgrep -c -u mysql mysqld) -eq 0 ]]; then
-       echo "running in a container..."
+# Check if mysql is already running, if not initialize
+if [[ $(pgrep -c -u mysql mysqld) -eq 0 ]]; then
+# Check if running in a container
+   if [ $DOCKER_CONTAINER  = true ]; then
+   # if in Docker container, avoid services that invoke the D-Bus
+       echo "ACA is running in a container..."
        /usr/bin/mysql_install_db
-       chown -R mysql:mysql /var/lib/mysql 
-       /usr/libexec/mysql-prepare-db-dir > /dev/null 2>&1 
+       chown -R mysql:mysql /var/lib/mysql/  
+       chown -R mysql:mysql /var/log/mariadb/
        nohup /usr/bin/mysqld_safe > /dev/null 2>&1 &
-    fi
-else
-    SQL_SERVICE=`/opt/hirs/scripts/common/get_db_service.sh`
-    systemctl $SQL_SERVICE enable
-    systemctl $SQL_SERVICE start
+   else
+       SQL_SERVICE=`/opt/hirs/scripts/common/get_db_service.sh`
+       systemctl $SQL_SERVICE enable
+       systemctl $SQL_SERVICE start
+   fi
+  # Set intial password
+  mysqladmin -u root password $DB_DEFAULT_PWD
 fi
+# Initialize the hirs_db database
 
-mysqladmin -u root password $DB_DEFAULT_PWD
 DB_CREATE_SCRIPT=/opt/hirs/scripts/common/db_create.sql.el7
-
 mysql -u root --password="$DB_DEFAULT_PWD" < $DB_CREATE_SCRIPT
