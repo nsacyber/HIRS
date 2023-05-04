9585513069d612ae0d56f82806359ec710747970
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2023-05-04 11:57:47 -0400

    Added in random UUID for unit test

--- HIRS_Utils/src/test/java/hirs/validation/SupplyChainCredentialValidatorTest.java
+++ HIRS_Utils/src/test/java/hirs/validation/SupplyChainCredentialValidatorTest.java
@@ -86,6 +86,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -1474,7 +1475,7 @@ public class SupplyChainCredentialValidatorTest {
         );
 
         Assert.assertTrue(
-                SupplyChainCredentialValidator.isMatch(null, pcComponentIdentifier,
+                SupplyChainCredentialValidator.isMatch(UUID.randomUUID(), pcComponentIdentifier,
                         nicComponentInfo)
         );
     }
