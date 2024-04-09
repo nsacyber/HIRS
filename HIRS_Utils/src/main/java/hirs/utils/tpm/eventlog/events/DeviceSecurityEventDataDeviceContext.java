4e54282ebe56922b5d68d3f5d5a51bb715baf935
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2024-04-09 16:23:59 -0400

    completed descriptions for data structures

--- HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/events/DeviceSecurityEventDataDeviceContext.java
+++ HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/events/DeviceSecurityEventDataDeviceContext.java
@@ -1,15 +1,15 @@
 package hirs.utils.tpm.eventlog.events;
 
 /**
- * Class to process the DeviceSecurityEventDataDeviceContext event
- * DEVICE_SECURITY_EVENT_DATA has 2 structures:
- *    1) DEVICE_SECURITY_EVENT_DATA_PCI_CONTEXT
- *    2) DEVICE_SECURITY_EVENT_DATA_USB_CONTEXT
+ * Class to process the DEVICE_SECURITY_EVENT_DATA_DEVICE_CONTEXT event per PFP.
+ * DEVICE_SECURITY_EVENT_DATA_DEVICE_CONTEXT is a common SPDM structure which includes the
+ * identification of the device, device vendor, subsystem, etc. Device can be either a PCI
+ * or USB connection.
  * <p>
- * typedef struct tdDEVICE_SECURITY_EVENT_DATA {
- * DEVICE_SECURITY_EVENT_DATA_PCI_CONTEXT       PciContext;
- * DEVICE_SECURITY_EVENT_DATA_PCI_CONTEXT       UsbContext;
- * } DEVICE_SECURITY_EVENT_DATA;
+ * typedef struct tdDEVICE_SECURITY_EVENT_DATA_DEVICE_CONTEXT {
+ *      DEVICE_SECURITY_EVENT_DATA_PCI_CONTEXT       PciContext;
+ *      DEVICE_SECURITY_EVENT_DATA_USB_CONTEXT       UsbContext;
+ * } tdDEVICE_SECURITY_EVENT_DATA_DEVICE_CONTEXT;
  * <p>
  */
 public class DeviceSecurityEventDataDeviceContext {
