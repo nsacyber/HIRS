72be21705ea46287f2ef8180167f2f863382bc23
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2024-04-09 16:23:59 -0400

    completed descriptions for data structures

--- HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/events/DeviceSecurityEventData.java
+++ HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/events/DeviceSecurityEventData.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 
 /**
- * Class to process the DeviceSecurityEventData event
+ * Class to process the DEVICE_SECURITY_EVENT_DATA event per PFP.
  * DEVICE_SECURITY_EVENT_DATA has 2 structures:
  *    1) DEVICE_SECURITY_EVENT_DATA_HEADER
- *    2) DEVICE_SECURITY_EVENT_DATA_PCI_CONTEXT
+ *    2) DEVICE_SECURITY_EVENT_DATA_DEVICE_CONTEXT
  * DEVICE_SECURITY_EVENT_DATA_HEADER
  *    The first 16 bytes of the event data header MUST be a String based identifier (Signature),
  *    NUL-terminated. The only currently defined Signature is "SPDM Device Sec" which implies
@@ -30,11 +30,11 @@ import java.util.List;
  * DEVICE_SECURITY_EVENT_DATA_DEVICE_CONTEXT    DeviceContext;
  * } DEVICE_SECURITY_EVENT_DATA;
  * <p>
- * Notes: Parses event data for an DEVICE_SECURITY_EVENT_DATA per PFP Spec.
+ * Notes: Parses event data for an DEVICE_SECURITY_EVENT_DATA per PFP v1.06 Rev52 Table 20.
  * 1. Has an EventType of EV_EFI_SPDM_FIRMWARE_BLOB (0x800000E1)
  * 2. Digest of 48 bytes
  * 3. Event content defined as DEVICE_SECURITY_EVENT_DATA Struct.
- * 4. First 16 bytes of the structure is an ASCII "SPDM Device Sec"
+ * 4. First 16 bytes of the structure header is an ASCII "SPDM Device Sec"
  */
 public class DeviceSecurityEventData {
 
