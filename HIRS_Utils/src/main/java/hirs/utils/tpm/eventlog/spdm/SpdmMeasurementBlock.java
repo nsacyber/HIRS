26c102c2054ee5f3543e7ce92fe0ed5d35077d64
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2024-04-18 17:08:36 -0400

    spdm processing

--- HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/spdm/SpdmMeasurementBlock.java
+++ HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/spdm/SpdmMeasurementBlock.java
@@ -1,8 +1,27 @@
 package hirs.utils.tpm.eventlog.spdm;
 
+import lombok.Getter;
+
 public class SpdmMeasurementBlock {
 
+    /**
+     * Measurement Spec.
+     */
+    @Getter
+    private String measurementSpec = "";
+    /**
+     * Measurement value type (such as mutable firmware, etc).
+     */
+    @Getter
+    private String dmtfSpecMeasurementValueType = "";
+    /**
+     * Measurement value (digest).
+     */
+    @Getter
+    private String dmtfSpecMeasurementValue = "";
+
     public SpdmMeasurementBlock(final byte[] spdmMeasBlockBytes) {
+
 
     }
 
