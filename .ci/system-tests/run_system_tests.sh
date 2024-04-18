85d47afd15b5bb494b023d53334f6d156d0ff7a8
iadgovuser62 <iadgovuser62@empire.eclipse.ncsc.mil>
2024-04-18 12:45:10 -0400

    Adding github actions components for systems test

--- .ci/system-tests/run_system_tests.sh
+++ .ci/system-tests/run_system_tests.sh
@@ -3,82 +3,38 @@
 #    Script to run the System Tests  for HIRS TPM 2.0 Provisoner
 #
 #########################################################################################
+# Uncomment the cd line below if running this script manually (To make Working directory = /HIRS/)
+# cd ../..
+
+# Setting variables
 aca_container=hirs-aca1
 tpm2_container=hirs-provisioner1-tpm2
-testResult="passed";
-issuerCert=../setup/certs/ca.crt
-hirs_aca_log=/var/log/tomcat/HIRS_AttestationCA.log
+. ./.ci/docker/.env && set -a
 
-# Source files for Docker Variables and helper scripts
-. ./.ci/docker/.env
-
-set -a
-
-echo "********  Setting up for HIRS System Tests for TPM 2.0 ******** "
-
-# Expand linux dmi files to mount to the provisioner container to simulate device component
-unzip -q .ci/system-tests/profiles/laptop/laptop_dmi.zip -d .ci/system-tests/profiles/laptop/
 # Start System Testing Docker Environment
-pushd .ci/docker > /dev/null
+echo "********  Setting up for HIRS System Tests for TPM 2.0 ******** "
+docker compose -f ./.ci/docker/docker-compose-system-test.yml up -d
 
-docker-compose -f docker-compose-system-test.yml up -d
-
-popd > /dev/null
-pushd .ci/system-tests > /dev/null
-source sys_test_common.sh
-
-# Build, Package, and Install HIRS ACA (2+ minutes) then wait for systems tests...
-docker exec $aca_container sh -c "/HIRS/.ci/setup/container/setup_aca.sh"
-echo "ACA Loaded!"
-echo "ACA Container info: $(checkContainerStatus $aca_container)";
-
-# Install HIRS provioner and setup tpm2 emulator
-docker exec $tpm2_container /HIRS/.ci/setup/container/setup_tpm2provisioner.sh
-echo "TPM2 Provisioner Container info: $(checkContainerStatus $tpm2_container)";
-
-# ********* Execute system tests here, add tests as needed ************* 
-echo "******** Setup Complete Begin HIRS System Tests ******** "
-
-source tests/aca_policy_tests.sh
-source tests/platform_cert_tests.sh
-source tests/rim_system_tests.sh
-
+# Install HIRS provisioner and setup tpm2 emulator
+docker exec $tpm2_container /.ci/setup/container/setup_tpm2provisioner_dotnet.sh
 echo "******** HIRS System Tests Complete ******** "
 
-# collecting ACA logs for archiving
-echo "Collecting ACA logs ....."
-docker exec $aca_container mkdir -p /HIRS/logs/aca/;
-docker exec $aca_container cp -a /var/log/tomcat/. /HIRS/logs/aca/;
-docker exec $aca_container chmod -R 777 /HIRS/logs/;
-echo "Collecting provisioner logs"
-docker exec $tpm2_container mkdir -p /HIRS/logs/provisioner/;
-docker exec $tpm2_container cp -a /var/log/hirs/provisioner/. /HIRS/logs/provisioner/;
-docker exec $tpm2_container chmod -R 777 /HIRS/logs/;
+# Collecting ACA and Provisioner.Net logs for workflow artifact
+echo "*** Extracting ACA and Provisioner.Net logs ..."
+docker exec $aca_container sh -c "cd .. && mkdir -p /HIRS/logs/aca/ && cp -arp /var/log/hirs/* /HIRS/logs/aca/"
+docker exec $tpm2_container sh -c "cd .. && mkdir -p /HIRS/logs/provisioner/ && cp -ap hirs*.log /HIRS/logs/provisioner/ && chmod -R 777 /HIRS/logs"
 
-echo ""
-echo "===========HIRS Tests and Log collection complete ==========="
-
-echo ""
-echo "End of System Tests for TPM 2.0, cleaning up..."
-echo ""
 # Clean up services and network
-popd > /dev/null
-pushd .ci/docker > /dev/null
-docker-compose -f docker-compose-system-test.yml down -v
-popd > /dev/null
-# Clean up dangling containers
-echo "Cleaning up dangling containers..."
-echo ""
-docker container prune --force
-echo ""
-echo "New value of test status is ${TEST_STATUS}"
+echo "*** Exiting and removing Docker containers and network ..."
+docker compose -f ./.ci/docker/docker-compose-system-test.yml down -v
+
 # Return container exit code
 if [[ ${TEST_STATUS} == "0" ]]; then
-    echo "SUCCESS: System Tests for TPM 2.0 passed"
+    echo "******** SUCCESS: System Tests for TPM 2.0 passed ********"
     echo "TEST_STATUS=0" >> $GITHUB_ENV
     exit 0;
   else
-    echo "FAILURE: System Tests for TPM 2.0 failed"
+    echo "******** FAILURE: System Tests for TPM 2.0 failed ********"
     echo "TEST_STATUS=1" >> $GITHUB_ENV
-    exit 1  
+    exit 1
 fi
