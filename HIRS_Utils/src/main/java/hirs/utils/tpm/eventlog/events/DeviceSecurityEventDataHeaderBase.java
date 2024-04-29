e5cb61d6987d5c3e471a29d86ca8ae7827934f87
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2024-04-29 08:49:07 -0400

    updated comments in ..HeaderBase to be more clear

--- HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/events/DeviceSecurityEventDataHeaderBase.java
+++ HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/events/DeviceSecurityEventDataHeaderBase.java
@@ -15,32 +15,39 @@ import java.nio.charset.StandardCharsets;
  * which implies the data is a DEVICE_SECURITY_EVENT_DATA or ..DATA2.
  *
  * HEADERS defined by PFP v1.06 Rev 52.
- * The ** indicates fields that are common to both ..HEADER and ..HEADER2.
+ * Certain fields are common to both ..HEADER and ..HEADER2, and are noted below the structures.
  * <p>
  * typedef struct tdDEVICE_SECURITY_EVENT_DATA_HEADER {
- *      UINT8                        ** Signature[16];
- *      UINT16                       ** Version;
+ *      UINT8                           Signature[16];
+ *      UINT16                          Version;
  *      UINT16                          Length;
  *      UINT32                          SpdmHashAlg;
- *      UINT32                       ** DeviceType;
+ *      UINT32                          DeviceType;
  *      SPDM_MEASUREMENT_BLOCK          SpdmMeasurementBlock;
- *      UINT64                       ** DevicePathLength;
- *      UNIT8                        ** DevicePath[DevicePathLength]
+ *      UINT64                          DevicePathLength;
+ *      UNIT8                           DevicePath[DevicePathLength]
  * } DEVICE_SECURITY_EVENT_DATA_HEADER;
  * <p>
  * typedef struct tdDEVICE_SECURITY_EVENT_DATA_HEADER2 {        - NOT IMPLEMENTED YET
- *      UINT8                        ** Signature[16];
- *      UINT16                       ** Version;
+ *      UINT8                           Signature[16];
+ *      UINT16                          Version;
  *      UINT8                           AuthState;
  *      UINT8                           Reserved;
  *      UINT32                          Length;
- *      UINT32                       ** DeviceType;
+ *      UINT32                          DeviceType;
  *      UINT32                          SubHeaderType;
  *      UINT32                          SubHeaderLength;
  *      UINT32                          SubHeaderUID;
- *      UINT64                       ** DevicePathLength;
- *      UNIT8                        ** DevicePath[DevicePathLength]
+ *      UINT64                          DevicePathLength;
+ *      UNIT8                           DevicePath[DevicePathLength]
  * } DEVICE_SECURITY_EVENT_DATA_HEADER2;
+ * <p>
+ * Fields common to both ..HEADER and ..HEADER2:
+ *    Signature
+ *    Version
+ *    DeviceType
+ *    DevicePathLength
+ *    DevicePath
  * <p>
  */
 public abstract class DeviceSecurityEventDataHeaderBase {
