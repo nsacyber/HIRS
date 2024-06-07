4d1c7a430fbd01da2a0c2ccdb89e6ae566d2f232
iadgovuser62 <iadgovuser62@empire.eclipse.ncsc.mil>
2024-06-07 09:38:01 -0400

    Adding Platform Cert Tests to V3 and modifying workflow file to split those tests into individual steps

--- .ci/system-tests/tests/platform_cert_tests.sh
+++ .ci/system-tests/tests/platform_cert_tests.sh
@@ -3,32 +3,43 @@
 #    HIRS Platform Certificate System Tests
 #
 #########################################################################################
+source ./.ci/system-tests/sys_test_common.sh
 testResult=false
 totalTests=0;
 failedTests=0;
+test="all"
+case $1 in
+    1) test="1" ;;
+    2) test="2" ;;
+    3) test="3" ;;
+esac
 
 # Start ACA Platform Certificate Tests
 # provisionTpm2 takes 1 parameter (the expected result): "pass" or "fail"
 # Note that the aca_policy_tests have already run several Platform Certificate system tests
 
-writeToLogs "### ACA PLATFORM CERTIFICATE TEST 1: Test a delta Platform Certificate that adds a new memory component ###"
-clearAcaDb
-uploadTrustedCerts
-setPolicyEkPc
-setPlatformCerts "laptop" "deltaPlatMem"
-provisionTpm2 "pass"
-
-writeToLogs "### ACA PLATFORM CERTIFICATE TEST 2: Test a Platform Certificate that is missing a memory component ###"
-clearAcaDb
-uploadTrustedCerts
-setPlatformCerts "laptop" "platCertLight"
-provisionTpm2 "pass"
-
-writeToLogs "### ACA PLATFORM CERTIFICATE TEST 3: Test a Delta Platform Certificate that has a wrong a memory component ###"
-clearAcaDb
-uploadTrustedCerts
-setPlatformCerts "laptop" "badDeltaMem"
-provisionTpm2 "fail"
+if [ "$test" = "1" ] || [ "$test" = "all" ]; then
+    writeToLogs "### ACA PLATFORM CERTIFICATE TEST 1: Test a delta Platform Certificate that adds a new memory component ###"
+    clearAcaDb
+    uploadTrustedCerts
+    setPolicyEkPc
+    setPlatformCerts "laptop" "deltaPlatMem"
+    provisionTpm2 "pass"
+fi
+if [ "$test" = "2" ] || [ "$test" = "all" ]; then
+    writeToLogs "### ACA PLATFORM CERTIFICATE TEST 2: Test a Platform Certificate that is missing a memory component ###"
+    clearAcaDb
+    uploadTrustedCerts
+    setPlatformCerts "laptop" "platCertLight"
+    provisionTpm2 "pass"
+fi
+if [ "$test" = "3" ] || [ "$test" = "all" ]; then
+    writeToLogs "### ACA PLATFORM CERTIFICATE TEST 3: Test a Delta Platform Certificate that has a wrong a memory component ###"
+    clearAcaDb
+    uploadTrustedCerts
+    setPlatformCerts "laptop" "badDeltaMem"
+    provisionTpm2 "fail"
+fi
 
 #  Process Test Results, any single failure will send back a failed result.
 if [[ $failedTests != 0 ]]; then
