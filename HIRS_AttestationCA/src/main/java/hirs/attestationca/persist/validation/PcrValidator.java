dbe6d0e5326f10fc774f944a7f3dcb9657f52dca
chubtub <43381989+chubtub@users.noreply.github.com>
2024-07-05 10:24:21 -0400

    Log calculatedString and quoteString for firmware validation

--- HIRS_AttestationCA/src/main/java/hirs/attestationca/persist/validation/PcrValidator.java
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/persist/validation/PcrValidator.java
@@ -217,6 +217,9 @@ public class PcrValidator {
               // other information.
             String calculatedString = Hex.encodeHexString(
                     pcrInfoShort.getCalculatedDigest());
+            log.debug("Validating PCR information with the following:" +
+                    System.lineSeparator() + "calculatedString = " + calculatedString +
+                    System.lineSeparator() + "quoteString = " + quoteString);
             validated = quoteString.contains(calculatedString);
             if (!validated) {
                 log.warn(calculatedString + " not found in " + quoteString);
