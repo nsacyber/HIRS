ac12e75403f23533adc29767ff921a69fdd50c3d
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2024-07-06 10:40:45 -0400

    Rim test 1 needed hw file

--- .ci/system-tests/container/pc_setup.sh
+++ .ci/system-tests/container/pc_setup.sh
@@ -19,6 +19,17 @@ hwJsonFileName="$profile"_"$test"_hw.json
 hwJsonFile="$testDir/$hwJsonFileName"
 ciTestHwJsonFile="$ciTestDir/hw.json"
 
+# Use default settings if profile does not have specific changes
+if [ ! -f "$hwJsonFile" ]; then
+    echo "Test is using a profile with no hardware manifest file. Using default."
+    hwJsonFile="$profileDir"/default/laptop_default_hw.json
+fi
+
+if [ ! -f "$dmiZip" ]; then
+    echo "Test is using a profile with no DMI data. Using default."
+    dmiZip="$profileDir"/default/laptop_dmi.zip
+fi
+
 # Current TCG folder for platform certs
 mkdir -p $tcgDir;  # Create the platform cert folder if its not there
 rm -f $tcgDir*;    # Clear out any previous data
@@ -26,13 +37,15 @@ rm -f $tcgDir*;    # Clear out any previous data
 echo "Test is using platform cert(s) from $profile : $test"
 # Step 1: Copy hw json file, if it exists.
 if [ -f "$hwJsonFile" ]; then
-      cp "$hwJsonFile" "$ciTestHwJsonFile"
+    echo "hw file used was $hwJsonFile"
+    cp "$hwJsonFile" "$ciTestHwJsonFile"
 fi
 
 # Can remove this once unzip is added to the image
 dnf install -y unzip &> /dev/null
 
 # Step 2: Unpack the dmi files.
+echo "dmi file used was $dmiZip"
 unzip -o "$dmiZip" -d "$ciTestDir"
 
 # Step 3: Copy the platform cert to tcg folder 
