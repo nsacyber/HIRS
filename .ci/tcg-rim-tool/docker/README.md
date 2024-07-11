bbae744cb6c640ef102ace036d23fae349f7fd69
SuperPotato27 <superpotatoman27@gmail.com>
2024-07-11 15:35:35 -0400

    deleted rim docker testing directory

--- .ci/tcg-rim-tool/docker/README.md
+++
@@ -1,8 +0,0 @@
-The script ```start_container.sh``` in this directory will run RIM tool tests found in
-```HIRS/.ci/tcg-rim-tool/scripts/``` in an Ubuntu docker container.
-
-To build the docker image and run the script:
-```
-docker build . -t <image_name>  
-./start_container.sh <image_name>
-```  
--- .ci/tcg-rim-tool/docker/container/rim_tests_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#install the rim tool.
-docker exec ubuntu_rim_testing /bin/bash -c "cd /hirsTemp/github/tools/tcg_rim_tool && ./gradlew clean Build && ./gradlew buildDeb"
-docker exec ubuntu_rim_testing /bin/bash -c "dpkg -i /hirsTemp/github/tools/tcg_rim_tool/build/distributions/tcg-rim-tool*.deb"
-
-#switch to specified branch (main).
-docker exec ubuntu_rim_testing /bin/bash -c "cd hirsTemp/github/HIRS && git checkout main"
-docker exec ubuntu_rim_testing /bin/bash -c "./hirsTemp/github/HIRS/.ci/tcg-rim-tool/scripts/run_all_tests.sh"
-
