0491df53cba18859f61a330ac3fcaa4996804709
ThatSilentCoder <184309164+ThatSilentCoder@users.noreply.github.com>
2025-09-26 09:53:04 -0400

    v3_issue_1008: Undid formatting changes that were auto applied by intellij to ps1 file

--- package/win/aca/aca_bootRun.ps1
+++ package/win/aca/aca_bootRun.ps1
@@ -1,50 +1,46 @@
 param (
     [string]$p, [string]$path = $null,
-    [switch]$w, [switch]$war = $false,
-    [switch]$d, [switch]$debug = $false,
-    [switch]$h, [switch]$help = $false
+	[switch]$w, [switch]$war = $false,
+	[switch]$d, [switch]$debug = $false,
+	[switch]$h, [switch]$help = $false
 )
 
 # Parameter Consolidation
-if ($p -and $path -and ($p -ne $path))
-{
-    "-p and -path were given different paths. Use only one." | WriteAndLog
-    $help = $true
+if ($p -and $path -and ($p -ne $path)) {
+	"-p and -path were given different paths. Use only one." | WriteAndLog
+	$help=$true
 }
-if ($p)
-{
-    $path = $p
+if ($p) {
+	$path = $p
 }
 $war = $w -or $war
 $debug = $d -or $debug
 $help = $h -or $help
 
-if ($help)
-{
-    Write-Output "  Bootrun script for the HIRS ACA"
-    Write-Output "  Syntax: powershell -ExecutionPolicy Bypass aca_bootRun.ps1 [-h|-p|-d|-w|-path|-war|-debug|-help]"
-    Write-Output "  options:"
-    Write-Output "     [ -p  | -path]   Path to the HIRS_AttestationCAPortal.war file"
-    Write-Output "     [ -w  | -war ]   Use deployed war file"
-    Write-Output "     [ -d  | -debug ]  Launch the JVM with a debug port open"
-    Write-Output "     [ -h  | -help ]   Print this help"
-    exit 1
+if ($help) {
+  Write-Output "  Bootrun script for the HIRS ACA"
+  Write-Output "  Syntax: powershell -ExecutionPolicy Bypass aca_bootRun.ps1 [-h|-p|-d|-w|-path|-war|-debug|-help]"
+  Write-Output "  options:"
+  Write-Output "     [ -p  | -path]   Path to the HIRS_AttestationCAPortal.war file"
+  Write-Output "     [ -w  | -war ]   Use deployed war file"
+  Write-Output "     [ -d  | -debug ]  Launch the JVM with a debug port open"
+  Write-Output "     [ -h  | -help ]   Print this help"
+  exit 1
 }
 
-if (!(New-Object Security.Principal.WindowsPrincipal(
-[Security.Principal.WindowsIdentity]::GetCurrent())
-).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-    Write-Output "This script requires root.  Please run as root"
-    exit 1
+if(!(New-Object Security.Principal.WindowsPrincipal(
+		[Security.Principal.WindowsIdentity]::GetCurrent())
+	).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	Write-Output "This script requires root.  Please run as root"
+	exit 1
 }
 
-$APP_HOME = (Split-Path -parent $PSCommandPath)
-$ACA_COMMON_SCRIPT = (Join-Path $APP_HOME 'aca_common.ps1')
-$ALG = "RSA" # or "ECC"
-$GRADLE_WRAPPER = './gradlew'
-$DEPLOYED_WAR = $null
-$DEBUG_OPTIONS = '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:9123'
+$APP_HOME=(Split-Path -parent $PSCommandPath)
+$ACA_COMMON_SCRIPT=(Join-Path $APP_HOME 'aca_common.ps1')
+$ALG="RSA" # or "ECC"
+$GRADLE_WRAPPER='./gradlew'
+$DEPLOYED_WAR=$null
+$DEBUG_OPTIONS='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:9123'
 
 # Load other scripts
 . $ACA_COMMON_SCRIPT
@@ -59,49 +55,38 @@ read_aca_properties $global:HIRS_DATA_ACA_PROPERTIES_FILE
 read_spring_properties $global:HIRS_DATA_SPRING_PROP_FILE
 
 Write-Output "-----------------------------------------------------------" | WriteAndLog
-Write-Output ("Running with these arguments: " + ($PSBoundParameters | Out-String)) | WriteAndLog
+Write-Output ("Running with these arguments: "+($PSBoundParameters | Out-String)) | WriteAndLog
 
-if ($path)
-{
+if ($path) {
     $DEPLOYED_WAR = $path
 }
 
-if (![System.IO.Directory]::Exists($global:HIRS_DATA_CERTIFICATES_HIRS_DIR))
-{
-    Write-Output "$global:HIRS_DATA_CERTIFICATES_HIRS_DIR directory does not exist. Please run .\aca_setup.ps1 and try again."
-    exit 1;
+if (![System.IO.Directory]::Exists($global:HIRS_DATA_CERTIFICATES_HIRS_DIR)) {
+     Write-Output "$global:HIRS_DATA_CERTIFICATES_HIRS_DIR directory does not exist. Please run .\aca_setup.ps1 and try again."
+     exit 1;
 }
 
-if (!$DEPLOYED_WAR)
-{
-    if (-not (Test-Path -Path $GRADLE_WRAPPER -PathType Leaf))
-    {
+if (!$DEPLOYED_WAR) {
+    if (-not (Test-Path -Path $GRADLE_WRAPPER -PathType Leaf)) {
         Write-Output 'This script is expected to be run from the HIRS top level project directory. Exiting.'
         exit 1;
     }
-    $DEPLOYED_WAR = './HIRS_AttestationCAPortal/build/libs/HIRS_AttestationCAPortal.war'
+	$DEPLOYED_WAR='./HIRS_AttestationCAPortal/build/libs/HIRS_AttestationCAPortal.war'
 }
 
-$SPRING_PROP_FILE_FORWARDSLASHES = ($global:HIRS_DATA_SPRING_PROP_FILE | ChangeBackslashToForwardSlash)
+$SPRING_PROP_FILE_FORWARDSLASHES=($global:HIRS_DATA_SPRING_PROP_FILE | ChangeBackslashToForwardSlash)
 
-if ($w -or $war)
-{
-    Write-Output "Booting the ACA from a war file..." | WriteAndLog
-    if ($d -or $debug)
-    {
+if ($w -or $war) {
+	Write-Output "Booting the ACA from a war file..." | WriteAndLog
+    if ($d -or $debug) {
         Write-Output "... in debug"
         java $DEBUG_OPTIONS -jar $DEPLOYED_WAR --spring.config.location=$SPRING_PROP_FILE_FORWARDSLASHES
+    } else {
+	    java -jar $DEPLOYED_WAR --spring.config.location=$SPRING_PROP_FILE_FORWARDSLASHES
     }
-    else
-    {
-        java -jar $DEPLOYED_WAR --spring.config.location=$SPRING_PROP_FILE_FORWARDSLASHES
-    }
-}
-else
-{
+} else  {
     Write-Output "Booting the ACA from local build..." | WriteAndLog
-    if ($d -or $debug)
-    {
+    if ($d -or $debug) {
         Write-Output "... in debug"
         ./gradlew :HIRS_UI:copyStaticFilesToBackEnd
         ./gradlew bootRun --args="--spring.config.location=$SPRING_PROP_FILE_FORWARDSLASHES" -Pdebug="$DEBUG_OPTIONS"
