54888ca077ffcf58cecbd202aa28c8eb82b98f9d
chubtub <43381989+chubtub@users.noreply.github.com>
2025-12-08 15:51:53 -0500

    Support -l use cases: 1. [no -l]: signature validation only 2. -l: signature validation and validation of declared support RIM 3. -l [directory]: signature validation and validation of support RIM matched under [directory]

--- tools/tcg_rim_tool/src/test/java/hirs/swid/TestSwidTagGateway.java
+++ tools/tcg_rim_tool/src/test/java/hirs/swid/TestSwidTagGateway.java
@@ -58,7 +58,6 @@ public class TestSwidTagGateway {
         gateway.setRimEventLog(SUPPORT_RIM_FILE);
         gateway.setAttributesFile(ATTRIBUTES_FILE);
         validator = new ReferenceManifestValidator();
-        validator.setRimEventLog(SUPPORT_RIM_FILE);
         validator.setTrustStoreFile(CA_CHAIN_FILE);
     }
 
