7497cba05e617e0b51f779bb8446ab94efb8339b
ThatSilentCoder <184309164+ThatSilentCoder@users.noreply.github.com>
2026-03-30 13:48:08 -0400

    v3.1_issue_1105: Fixed comments made in the javadocs.

--- HIRS_AttestationCA/src/main/java/hirs/attestationca/persist/entity/manager/ComponentResultRepository.java
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/persist/entity/manager/ComponentResultRepository.java
@@ -20,28 +20,30 @@ import java.util.UUID;
 public interface ComponentResultRepository extends JpaRepository<ComponentResult, UUID> {
 
     /**
-     * Query based on the device serial number.
+     * Query that retrieves a list of {@link ComponentResult} objects based on the device serial number.
      *
      * @param boardSerialNumber variable holding the device serial number
-     * @return a list of component result.
+     * @return a list of {@link ComponentResult} objects
      */
     List<ComponentResult> findByBoardSerialNumber(String boardSerialNumber);
 
     /**
-     * Query based on the device serial number.
+     * Query that retrieves a list of {@link ComponentResult} objects based on the device serial number and a boolean
+     * value.
      *
      * @param boardSerialNumber variable holding the device serial number
      * @param delta             flag indicating if the component is associated with a delta certificate
-     * @return a list of component result.
+     * @return a list of {@link ComponentResult} objects
      */
     List<ComponentResult> findByBoardSerialNumberAndDelta(String boardSerialNumber, boolean delta);
 
     /**
-     * Query based on certificate serial number and device serial number.
+     * Query that retrieves a list of {@link ComponentResult} objects based on certificate serial number
+     * and device serial number.
      *
      * @param certificateSerialNumber certificate specific serial number
      * @param boardSerialNumber       variable holding the device serial number
-     * @return a list of component result.
+     * @return a list of {@link ComponentResult} objects
      */
     List<ComponentResult> findByCertificateSerialNumberAndBoardSerialNumber(
             String certificateSerialNumber, String boardSerialNumber);
--- HIRS_AttestationCA/src/main/java/hirs/attestationca/persist/entity/manager/ReferenceDigestValueRepository.java
+++ HIRS_AttestationCA/src/main/java/hirs/attestationca/persist/entity/manager/ReferenceDigestValueRepository.java
@@ -20,35 +20,35 @@ import java.util.UUID;
 public interface ReferenceDigestValueRepository extends JpaRepository<ReferenceDigestValue, UUID> {
 
     /**
-     * Query that retrieves a list of {@link ReferenceDigestValue} object using the provided associated rim id.
+     * Query that retrieves a list of {@link ReferenceDigestValue} objects using the provided associated rim id.
      *
      * @param associatedRimId uuid representation of the associated rim ID
-     * @return a list of {@link ReferenceDigestValue} object
+     * @return a list of {@link ReferenceDigestValue} objects
      */
     List<ReferenceDigestValue> findValuesByBaseRimId(UUID associatedRimId);
 
     /**
-     * Query that retrieves a list of {@link ReferenceDigestValue} object using the provided support rim id.
+     * Query that retrieves a list of {@link ReferenceDigestValue} objects using the provided support rim id.
      *
      * @param supportRimId uuid representation of the support rim ID
-     * @return a list of {@link ReferenceDigestValue} object
+     * @return a list of {@link ReferenceDigestValue} objects
      */
     List<ReferenceDigestValue> findBySupportRimId(UUID supportRimId);
 
     /**
-     * Query that retrieves a list of {@link ReferenceDigestValue} object using the provided support rim hash.
+     * Query that retrieves a list of {@link ReferenceDigestValue} objects using the provided support rim hash.
      *
      * @param supportRimHash a string representation of the support rim hash
-     * @return a list of {@link ReferenceDigestValue} object
+     * @return a list of {@link ReferenceDigestValue} objects
      */
     List<ReferenceDigestValue> findBySupportRimHash(String supportRimHash);
 
     /**
-     * Query that retrieves a list of {@link ReferenceDigestValue} object using the provided manufacturer and model.
+     * Query that retrieves a list of {@link ReferenceDigestValue} objects using the provided manufacturer and model.
      *
      * @param manufacturer string representation of the manufacturer
      * @param model        string representation of the model
-     * @return a list of {@link ReferenceDigestValue} object
+     * @return a list of {@link ReferenceDigestValue} objects
      */
     List<ReferenceDigestValue> findByManufacturerAndModel(String manufacturer, String model);
 }
