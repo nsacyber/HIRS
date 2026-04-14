02e6c426135030c4fd5b8f9270b3a48e315a694a
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2026-04-14 11:51:31 -0400

    updating eventlog list # scheme

--- HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/TCGEventLog.java
+++ HIRS_Utils/src/main/java/hirs/utils/tpm/eventlog/TCGEventLog.java
@@ -221,26 +221,27 @@ public final class TCGEventLog {
         bEvent = bEventFlag;
         bHexEvent = bHexEventFlag;
 
+        int eventNumber = 0;
         ByteArrayInputStream is = new ByteArrayInputStream(rawlog);
 
         try {
             // Process the 1st entry as a SHA1 format (per the spec) and put into the event list
-            TpmPcrEvent1 firstEvent = new TpmPcrEvent1(is, eventNumber++);
-            eventList.put(eventNumber, firstEvent);
+            TpmPcrEvent1 firstEvent = new TpmPcrEvent1(is, eventNumber);
+            eventList.put(eventNumber++, firstEvent);
             useFirstEventToInitValues(firstEvent);
 
             // put the remaining events into the event list
             while (is.available() > 0) {
                 if (bCryptoAgile) {
-                    TpmPcrEvent2 event2 = new TpmPcrEvent2(is, eventNumber++, strongestEvLogHashAlgName);
-                    eventList.put(eventNumber, event2);
+                    TpmPcrEvent2 event2 = new TpmPcrEvent2(is, eventNumber, strongestEvLogHashAlgName);
+                    eventList.put(eventNumber++, event2);
                     if (event2.isStartupLocalityEvent()) {
                         EvNoAction event = new EvNoAction(event2.getEventContent());
                         startupLocality = event.getStartupLocality();
                     }
                 } else {
-                    TpmPcrEvent1 event1 = new TpmPcrEvent1(is, eventNumber++);
-                    eventList.put(eventNumber, event1);
+                    TpmPcrEvent1 event1 = new TpmPcrEvent1(is, eventNumber);
+                    eventList.put(eventNumber++, event1);
                 }
 
                 // first check if any previous event has not been able to access vendor-table.json,
